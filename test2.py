a58f88e10557282089ab10255cfe3aa48abd0bed
lynethmendoza <123670042+lynethmendoza@users.noreply.github.com>
2024-01-08 15:55:36 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -40,11 +40,25 @@ class Processor:
     self.currentKey = false;
   	self.delayTimer = 0;
   	self.soundTimer = 0;
+
+  #BASIC INSTRUCTIONS (first nibble -- four bits)
   #00E0 clear screen (set display array to all 0) 
   #1NNN = Jump, set PC to whatever NNN is
   #2NNN = Will call the subroutine in location NNN, pop the last address in the stack and make that the PC
   #00EE will return back to the opcodes (like RET in assembly)
-  #3XNN is used as skip 
+  #3XNN will skip one instruction if the value in VX (variable X) is equal to NN
+  #4XNN will do the same as 3XNN but if they are not equal
+  #the ones above are compared to immediate values
+  #5XY0 will skip one instruction if VX (variable X) and VY (variable Y) are equal
+  #9XY0 will do the same  as 5XY0 but if they are not equal
+  #these are comparing variables in registers
+  #6XNN will set the value in variable X to whatever NN is
+  #7XNN will add the value of variable X to whatever NN is
+
+  #LOGICAL INSTRUCTIONS (after the first nibble, last nibble of the opcode)
+  #8XY0 will set the value in variable X to the value variable Y
+  #
+
     
   def say_hi(self):
     print('Hello, my name is', self.name)
