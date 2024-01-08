ca40d8446df4b78df7332f45d0343fb7d8a5fe44
Jeshua-Benzant <78059200+Jeshua-Benzant@users.noreply.github.com>
2024-01-07 20:18:33 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,5 +5,5 @@ class Processor:
   def __init__(self,name):
     self.name = name
     
-    def say_hi(self):
-        print('Hello, my name is', self.name)
+  def say_hi(self):
+    print('Hello, my name is', self.name)
